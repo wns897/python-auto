bd9d135ba16386837e123f5b2ca4d8e1f1f10908
wns897 <wns897@naver.com>
2021-04-09 01:48:49 +0900

    commit

--- crawling.py
+++ crawling.py
@@ -1,8 +1,17 @@
 import urllib.request
+import urllib.parse
 from bs4 import BeautifulSoup
 
-url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%ED%8C%8C%EC%9D%B4%EC%8D%AC'
+baseUrl = 'https://search.naver.com/search.naver?where=post&sm=tab_jum&query='
+plusUrl = input('검색어를 입력하세요:')
+url = baseUrl + urllib.parse.quote_plus(plusUrl)
+print(url)
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 
-print(soup)
+title = soup.find_all(class_='sh_blog_title')
+
+for i in title:
+    print(i.attrs['title'])
+    print(i.attrs['href'])
+    print()
